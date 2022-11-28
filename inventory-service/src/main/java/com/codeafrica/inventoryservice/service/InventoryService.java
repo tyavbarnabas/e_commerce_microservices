b9e92f69809ba85964ef51623e5944068aae0a40
beny1122war <tyavbarnabas2@gmail.com>
2022-11-28 22:46:22 +0100

    implemented resilience microservices using resilience4j

--- inventory-service/src/main/java/com/codeafrica/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/codeafrica/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.codeafrica.inventoryservice.service;
 import com.codeafrica.inventoryservice.dto.InventoryResponse;
 import com.codeafrica.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +14,16 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode){
+    public List<InventoryResponse> isInStock(List<String> skuCode) throws InterruptedException {
+        log.info("wait started");
+        Thread.sleep(1000);
+        log.info("wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
